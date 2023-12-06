1d3dec5f12c7c816377063937bb0061eb7e72620
Joren 's Jongers <36844725+jorensjongers@users.noreply.github.com>
2023-12-06 23:56:31 +0100

    Move copy to translation file (#1)

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,59 @@
-module Main exposing (main, view)
+module Main exposing (Model, Msg, main, view)
 
 import Browser
 import Html exposing (..)
+import Http
+import I18Next exposing (Translations, t)
 
 
-main : Program () () Never
+fetchTranslations : String -> Cmd Msg
+fetchTranslations path =
+    Http.get
+        { url = path
+        , expect = Http.expectJson FetchTranslations I18Next.translationsDecoder
+        }
+
+
+main : Program () Model Msg
 main =
     Browser.document
-        { init = \_ -> ( (), Cmd.none )
+        { init = init
         , subscriptions = \_ -> Sub.none
-        , update = \_ -> \_ -> ( (), Cmd.none )
+        , update = update
         , view = view
         }
 
 
-view : () -> Browser.Document Never
-view () =
-    { title = "Hello world!"
+type Msg
+    = FetchTranslations (Result Http.Error Translations)
+
+
+type alias Model =
+    { translations : I18Next.Translations
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { translations = I18Next.initialTranslations }, fetchTranslations "/i18n/en-GB.json" )
+
+
+view : Model -> Browser.Document Msg
+view { translations } =
+    { title = t translations "helloWorld"
     , body =
         [ div []
-            [ text "Hello world!" ]
+            [ text (t translations "helloWorld") ]
         ]
     }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        -- ...
+        FetchTranslations (Ok translations) ->
+            ( { model | translations = translations }, Cmd.none )
+
+        FetchTranslations (Err _) ->
+            ( model, Cmd.none )
